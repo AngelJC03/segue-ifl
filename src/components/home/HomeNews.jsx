c75c0ff34ade64ec1c59bac7cce877f0d44e9ddb
angel <CAngel080803@gmail.com>
2025-07-23 19:16:30 -0400

    something went horribly wrong

--- src/components/home/HomeNews.jsx
+++ src/components/home/HomeNews.jsx
@@ -1,22 +1,35 @@
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './HomeNews.css'; // Import the testimonials CSS
-// import { Button } from '../navbar/Button'; //
-// import testImage from '../../assets/images/testFolder/testNews.jpg'; // Import the test image
+
 
 function HomeNews() {
+    const API = 'https://funny-kheer-222f19.netlify.app/.netlify/functions/getSheet';
+
+    const [news, setNews] = useState([]);
+    useEffect(() => {
+        fetch(API)
+            .then((response) => response.json())
+            .then((data) => {
+                setNews(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    console.log(news);
+
     return (
         <div className="home-news-container">
             <div className="home-news-section">
                 <h2>Latest News</h2>
-                <div className="home-news-text">
-                    this is a test for the home news section
-                </div>
-                <div className="home-news-text">
-                    this is a test for the home news section
-                </div>
-                <div className="home-news-text">
-                    this is a test for the home news section
-                </div>
+                {news.map((item, index) => (
+                    <div key={index} className="home-news-text">
+                        <h3>{item.title}</h3>
+                        <p>{item.description}</p>
+                        <p><strong>Date:</strong> {new Date(item.date).toLocaleDateString()}</p>
+                    </div>
+                ))}
             </div>
             <div className="facebook-livefeed-container">
                 <h2>Facebook Feed</h2>
