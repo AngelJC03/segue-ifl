d9b8a83cc0c7625486e7c58aae0a2cf9b0b18359
angel <CAngel080803@gmail.com>
2025-07-23 21:59:31 -0400

    testing fb functions and its going horribly

--- src/components/home/HomeNews.jsx
+++ src/components/home/HomeNews.jsx
@@ -39,18 +39,6 @@ function HomeNews() {
             });
     }, []);
 
-    //test facebook function
-    useEffect(() => {
-        fetch('/.netlify/functions/getFacebookposts')
-            .then(response => response.json())
-            .then(data => {
-                console.log('Facebook posts:', data);
-            })
-            .catch(error => {
-                console.error('Error fetching Facebook posts:', error);
-            });
-    }, []);
-
     return (
         <div className="home-news-container">
             <div className="home-news-section">
